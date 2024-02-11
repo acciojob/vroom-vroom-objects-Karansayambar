92c58aef50688dcf2e216e3a66154997d3295133
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-11 09:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
-Car.prototype.getMakeModel function(){
+Car.prototype.getMakeModel = function(){
 	return `${this.make} ${this.model}`;
 };
 
@@ -15,7 +15,7 @@ function SportsCar(make, model, topSpeed) {
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = function{
+SportsCar.prototype.getTopSpeed = function(){
 	return this.topSpeed;
 }
 const car = new SportsCar("Ferrari", "Testarossa", 200);
